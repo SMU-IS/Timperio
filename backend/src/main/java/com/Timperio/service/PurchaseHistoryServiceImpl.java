1dbc06463302ff38239fbfadf373be610d7ede6f
joshydavid <joshuadavidang@outlook.sg>
2024-10-22 10:51:20 +0800

    refactor: code clean up
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>

--- backend/src/main/java/com/Timperio/service/PurchaseHistoryServiceImpl.java
+++ backend/src/main/java/com/Timperio/service/PurchaseHistoryServiceImpl.java
@@ -16,22 +16,27 @@ public class PurchaseHistoryServiceImpl implements PurchaseHistoryService {
     @Autowired
     private PurchaseHistoryRepository purchaseHistoryRepository;
 
+    @Override
     public Iterable<PurchaseHistory> findAll() {
         return purchaseHistoryRepository.findAll();
     }
 
+    @Override
     public Iterable<PurchaseHistory> findByCustomerId(Integer id) {
         return purchaseHistoryRepository.findByCustomerId(id);
     }
 
+    @Override
     public Iterable<PurchaseHistory> findBySalesType(SalesType salesType) {
         return purchaseHistoryRepository.findBySalesType(salesType);
     }
 
+    @Override
     public Iterable<PurchaseHistory> findByChannelType(ChannelType channelType) {
         return purchaseHistoryRepository.findByChannelType(channelType);
     }
 
+    @Override
     public Iterable<PurchaseHistory> findByShippingMethod(ShippingMethod shippingMethod) {
         return purchaseHistoryRepository.findByShippingMethod(shippingMethod);
     }
